9f3cec685c12046c1ae5730d565bcbde9a3ad55c
Matias Arias <matias.arias1011@gmail.com>
2023-04-21 15:48:07 -0400

    listo

--- scr.js
+++ scr.js
@@ -1,19 +1,20 @@
 let personajeA = {
     nombre: "A",
     vida: 30,
-    ataque: 2
+    ataque: 1
 }
 
 let personajeB = {
     nombre: "B",
     vida: 30,
-    ataque: 10
+    ataque: 1
 }
 
 while(personajeA.vida > 0 && personajeB.vida > 0){
-    console.log(personajeA.vida +" "+personajeB.vida);
-    personajeA.vida=-personajeB.ataque;
-    personajeB.vida=-personajeA.ataque;
+    console.log(personajeA.vida);
+    console.log(personajeB.vida);
+    personajeA.vida-=personajeB.ataque;
+    personajeB.vida-=personajeA.ataque;
 }
 
 console.log(personajeA);
